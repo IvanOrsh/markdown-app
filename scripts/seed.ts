97f7bd51c9814b1c30f083b8cc3ad5c0b83121f2
IvanOrsh <ksironov@mail.ru>
2023-12-04 22:20:22 +0300

    chore: seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,13 +1,66 @@
+import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
+
 import { getClient } from "@/app/lib/server/db";
 
 async function seed() {
   const client = getClient();
   await client.connect();
 
-  const res = await client.query("select 1");
-  console.log(res.rows);
+  await client.query("begin");
+  try {
+    const saltRounds = 10;
+    const hash = await bcrypt.hash("123123", saltRounds);
 
-  await client.end();
+    console.log("Creating users...");
+
+    await client.query(
+      "insert into users (username, password) values ($1, $2) on conflict do nothing",
+      ["demo", hash]
+    );
+
+    const demoUserRes = await client.query(
+      "select id from users where username = 'demo'"
+    );
+    const demoUser = demoUserRes.rows[0];
+
+    for (let i = 0; i < 10; i++) {
+      console.log(`create note ${i} for demo user`);
+      await client.query(
+        "insert into notes (user_id, title, content) values ($1, $2, $3)",
+        [demoUser.id, faker.lorem.sentence(), faker.lorem.paragraphs()]
+      );
+    }
+
+    for (let i = 0; i < 10; i++) {
+      console.log(`crete user ${i}`);
+      await client.query(
+        "insert into users (username, password) values ($1, $2)",
+        [faker.internet.userName(), hash]
+      );
+    }
+
+    const usersRes = await client.query(
+      "select id from users order by created_at desc limit 10"
+    );
+
+    for (const row of usersRes.rows) {
+      for (let i = 0; i < 10; i++) {
+        console.log(`create note ${i} for user ${row.id}`);
+        await client.query(
+          "insert into notes (user_id, title, content) values ($1, $2, $3)",
+          [row.id, faker.lorem.sentence(), faker.lorem.paragraphs()]
+        );
+      }
+    }
+
+    await client.query("commit");
+  } catch (e) {
+    await client.query("rollback");
+    console.error(e);
+  } finally {
+    client.end();
+  }
 }
 
 seed();
