d650556df87f43e7bdfa15166a3d6d6e0855449f
IvanOrsh <ksironov@mail.ru>
2023-12-04 23:16:22 +0300

    chore: scaffold home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,31 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="space-y-2 w-full">
+      <h1 className="bg-yellow-300 p-2 font-bold text-black w-full text-center">
+        Markdown App
+      </h1>
+
+      <ul className="space-y-2">
+        <li>
+          <Link
+            className="block bg-red-700 p-2 text-white w-full text-center hover:outline hover:outline-2 hover:outline-red-900"
+            href="/signup"
+          >
+            Sign Up
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            className="block bg-blue-700 p-2 text-white w-full text-center hover:outline hover:outline-2 hover:outline-blue-900"
+            href="/login"
+          >
+            Log In
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
