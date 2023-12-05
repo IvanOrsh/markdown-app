b81d6d6469a6eff13a65bfda02f7b97d3b64e8a6
IvanOrsh <ksironov@mail.ru>
2023-12-05 20:55:40 +0300

    chore: signup server action

---
+++ src/app/(public)/signup/actions.ts
@@ -0,0 +1,99 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
+
+import { sql } from "@/app/lib/server/db";
+import config from "@/app/lib/server/config";
+
+const SignUpSchema = z.object({
+  username: z.string().min(1),
+  password: z.string().min(6),
+  confirmPassword: z.string().min(6),
+});
+
+export type State = {
+  errors?: {
+    username?: string;
+    password?: string;
+    confirmPassword?: string;
+  };
+  message?: string;
+};
+
+export async function signup(prevState: State, formData: FormData) {
+  const validatedFields = SignUpSchema.safeParse({
+    username: formData.get("username"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.formErrors.fieldErrors,
+      message: "Sign up error",
+    };
+  }
+
+  const username = formData.get("username");
+  const password = formData.get("password");
+  const confirmPassword = formData.get("confirmPassword");
+
+  if (password !== confirmPassword) {
+    return {
+      errors: {
+        confirmPassword: "Password and Confirm Password must match",
+      },
+      message: "Sign up error",
+    };
+  }
+
+  const userRes = await sql("select * from users where username = $1", [
+    username,
+  ]);
+
+  // user already exists
+  if (userRes.rowCount && userRes.rowCount > 0) {
+    return {
+      errors: {
+        username: "Username already exists",
+      },
+      message: "Sign up error",
+    };
+  }
+
+  const saltRounds = 10;
+  const hash = await bcrypt.hash(password?.toString()!, saltRounds);
+
+  const insertRes = await sql(
+    "insert into users (username, password) values ($1, $2) return *",
+    [username, hash]
+  );
+
+  // happy - generate JWT & cookie
+  if (insertRes.rowCount === 1) {
+    const user = insertRes.rows[0];
+    const token = await new SignJWT({ id: user.id })
+      .setProtectedHeader({ alg: "HS256" })
+      .setSubject(user.id)
+      .setIssuedAt()
+      .setExpirationTime("2w")
+      .sign(new TextEncoder().encode(config.JWT_SECRET));
+
+    cookies().set("token", token, {
+      sameSite: "strict",
+      httpOnly: true,
+      secure: true,
+    });
+
+    redirect("/dashboard");
+  }
+
+  // totally unexpected
+  return {
+    message: "Unexpected error",
+  };
+}
