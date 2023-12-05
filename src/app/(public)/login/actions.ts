c03bafe19e8d214d5281853e39fe14233023857e
IvanOrsh <ksironov@mail.ru>
2023-12-05 20:15:04 +0300

    feat: login server action

---
+++ src/app/(public)/login/actions.ts
@@ -0,0 +1,78 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
+
+import { sql } from "@/app/lib/server/db";
+import config from "@/app/lib/server/config";
+
+const LoginSchema = z.object({
+  username: z.string().min(1),
+  password: z.string().min(6),
+});
+
+type State = {
+  errors?: {
+    username?: string;
+    password?: string;
+  };
+
+  message?: string | null;
+};
+
+export async function login(prevState: State, formData: FormData) {
+  const validatedFields = LoginSchema.safeParse({
+    username: formData.get("username"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Log in error",
+    };
+  }
+
+  const username = formData.get("username");
+  const password = formData.get("password");
+
+  // check if this user actually exists
+  const userRes = await sql("select * from users where username = $1", [
+    username,
+  ]);
+  if (userRes.rowCount === 0) {
+    return {
+      message: "User not found",
+    };
+  }
+
+  const user = userRes.rows[0];
+
+  const isMatch = await bcrypt.compare(password?.toString()!, user.password);
+
+  // happy
+  if (isMatch) {
+    const token = await new SignJWT({})
+      .setProtectedHeader({ alg: "HS256" })
+      .setSubject(user.id)
+      .setIssuedAt()
+      .setExpirationTime("2w")
+      .sign(new TextEncoder().encode(config.JWT_SECRET));
+
+    cookies().set("jwt-token", token, {
+      sameSite: "strict",
+      httpOnly: true,
+      secure: true,
+    });
+
+    redirect("/dashboard");
+  }
+
+  // otherwise
+  return {
+    message: "Credentials invalid",
+  };
+}
