bda75957f449995fb5446a99417e4a06544385b2
IvanOrsh <ksironov@mail.ru>
2023-12-07 23:35:02 +0300

    feat: detect search params change in content container

--- src/app/dashboard/@content/page.tsx
+++ src/app/dashboard/@content/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Page() {
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    console.log(searchParams);
+    const note_id = searchParams.get("note_id");
+    console.log("content container:", note_id);
+  }, [searchParams]);
+
   return (
     <div>
       <h1>Content</h1>
