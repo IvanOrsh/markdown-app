2e6e52f4abf9036ff94919a400cd474049a98be9
IvanOrsh <ksironov@mail.ru>
2023-12-07 20:54:00 +0300

    feat: recursive components

--- src/app/ui/note-list.tsx
+++ src/app/ui/note-list.tsx
@@ -3,14 +3,20 @@ import Note from "./note";
 
 type NoteListProps = {
   notes: NoteData[];
+  depth?: number;
 };
 
-export default function NoteList({ notes }: NoteListProps) {
+export default function NoteList({ notes, depth = 0 }: NoteListProps) {
   return (
-    <ul className="space-y-4">
+    <ul
+      className={`space-y-4 `}
+      style={{
+        marginLeft: `${depth * 10}px`,
+      }}
+    >
       {notes.map((note) => (
         <li key={note.id}>
-          <Note note={note} />
+          <Note note={note} depth={depth + 1} />
         </li>
       ))}
     </ul>
--- src/app/ui/note.tsx
+++ src/app/ui/note.tsx
@@ -1,9 +1,17 @@
 import { DateTime } from "luxon";
 
 import { NoteData } from "../lib/client/types";
-import { useNotesDispatch } from "../contexts/notes-context";
+import { useNotesDispatch, useNotesState } from "../contexts/notes-context";
+import { updateParent } from "../lib/client/api";
+import NoteList from "./note-list";
 
-export default function Note({ note }: { note: NoteData }) {
+type NoteProps = {
+  note: NoteData;
+  depth: number;
+};
+
+export default function Note({ note, depth }: NoteProps) {
+  const state = useNotesState();
   const dispatch = useNotesDispatch();
 
   function handleDragStart(e: React.DragEvent) {
@@ -19,8 +27,28 @@ export default function Note({ note }: { note: NoteData }) {
     console.log("drag end");
   }
 
-  function handleDrop(e: React.DragEvent) {
+  async function handleDrop(e: React.DragEvent) {
     console.log("drop", note.id);
+    // #3
+    console.log("current drag id:", state.currentDragId);
+
+    // make sure we are not dropping node to itself
+    if (note.id === state.currentDragId) {
+      alert("cannot move note into itself");
+      return;
+    }
+
+    // TODO: check if target note is descendent of current dragging note
+
+    // update parent api call
+    await updateParent(state.currentDragId!, note.id);
+
+    // dispatch change_parent event
+    dispatch({
+      type: "change_parent",
+      newParentId: note.id,
+      currentDragId: state.currentDragId,
+    });
   }
 
   function handleDragOver(e: React.DragEvent) {
@@ -37,40 +65,46 @@ export default function Note({ note }: { note: NoteData }) {
   }
 
   return (
-    <article
-      draggable
-      onDragStart={handleDragStart}
-      onDragEnd={handleDragEnd}
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      className="p-2 bg-white text-black border-2 hover:outline hover:outline-2 hover:outline-black rounded group  cursor-pointer space-y-2"
-    >
-      <h2 className="text-xl font-bold line-clamp-1 group-hover:line-clamp-none ">
-        {note.title}
-      </h2>
-      <p className="hidden group-hover:block text-sm text-gray-500 dark:text-gray-300">
-        id: {note.id}
-      </p>
-      <p className="hidden group-hover:block">
-        <span className="font-bold text-gray-500 dark:text-gray-300 text-sm mr-2">
-          created:
-        </span>
-        {note.created_at.toLocaleString(DateTime.DATETIME_MED)}
-      </p>
-      <p>
-        <span className="font-bold text-gray-500 dark:text-gray-300 text-sm mr-2">
-          updated:
-        </span>
-        {note.updated_at.toLocaleString(DateTime.DATETIME_MED)}
-      </p>
-      <p>
-        <span className="font-bold text-gray-500 dark:text-gray-300 text-sm mr-2">
-          status:
-        </span>
-        {note.is_published ? "Published" : "Draft"}
-      </p>
-    </article>
+    <div>
+      <article
+        draggable
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        className="p-2 bg-white text-black border-2 hover:outline hover:outline-2 hover:outline-black rounded group  cursor-pointer space-y-2"
+      >
+        <h2 className="text-xl font-bold line-clamp-1 group-hover:line-clamp-none ">
+          {note.title}
+        </h2>
+        <p className="hidden group-hover:block text-sm text-gray-500 dark:text-gray-300">
+          id: {note.id}
+        </p>
+        <p className="hidden group-hover:block">
+          <span className="font-bold text-gray-500 dark:text-gray-300 text-sm mr-2">
+            created:
+          </span>
+          {note.created_at.toLocaleString(DateTime.DATETIME_MED)}
+        </p>
+        <p>
+          <span className="font-bold text-gray-500 dark:text-gray-300 text-sm mr-2">
+            updated:
+          </span>
+          {note.updated_at.toLocaleString(DateTime.DATETIME_MED)}
+        </p>
+        <p>
+          <span className="font-bold text-gray-500 dark:text-gray-300 text-sm mr-2">
+            status:
+          </span>
+          {note.is_published ? "Published" : "Draft"}
+        </p>
+      </article>
+
+      {note.child_notes?.length > 0 && (
+        <NoteList notes={note.child_notes} depth={depth + 1} />
+      )}
+    </div>
   );
 }
