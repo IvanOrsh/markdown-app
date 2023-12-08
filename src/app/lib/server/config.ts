883d87520d9802ec32ea03b0bb2813ca68c62fdc
IvanOrsh <ksironov@mail.ru>
2023-12-08 22:04:31 +0300

    fix: typo

--- src/app/lib/server/config.ts
+++ src/app/lib/server/config.ts
@@ -4,7 +4,7 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 const config = {
-  POSTGRES_URL: process.env.DATABASE_URL,
+  POSTGRES_URL: process.env.POSTGRES_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   ENV: process.env.ENV || "development",
 };
